08297c9444f82f3f5e97912b62083257e920c6a1
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-06 04:26:23 +0100

    Update Images.py

--- sr_api/Images.py
+++ sr_api/Images.py
@@ -1,24 +1,24 @@
 import aiohttp
 import json
 
-async def __call_api(self, endpoint):
-    async with aiohttp.ClientSession() as c:
-        async with c.get(f'https://some-random-api.ml/{endpoint}') as r:
-            return await r.json()
-
-async def __get_api_img(img):
-    base = await __call_api(f'img/{img}')
-    return base['link']
-
 class Image:
+    
+    async def __call_api(self, endpoint):
+        async with aiohttp.ClientSession() as c:
+            async with c.get(f'https://some-random-api.ml/{endpoint}') as r:
+                return await r.json()
+
+    async def __get_api_img(img):
+        base = await __call_api(f'img/{img}')
+        return base['link']
 
     @property
     def dog(self=None):
-        return await __get_api_img('dog')
+        return await self.__get_api_img('dog')
 
     @property
     def cat(self=None):
-        return __get_api_img('cat')
+        return self.__get_api_img('cat')
 
     @property
     def bird(self=None):
