19a7762202103bdc58cb0e65e2bc3ddc01669d68
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-06 03:41:43 +0100

    Create Main.py

---
+++ Api/Main.py
@@ -0,0 +1,28 @@
+import aiohttp
+import json
+
+from Pokedex import Pokedex
+from Images import Image
+
+class ApiError(Exception):
+    pass
+
+class InputError(Exception):
+    pass
+
+
+class ApiClient:
+
+    async def __call_api(self, endpoint):
+        async with aiohttp.ClientSession() as c:
+            async with c.get(f'https://some-random-api.ml/{endpoint}') as r:
+                return await r.json()
+
+    async def get_pokemon(self, name):
+        result = await self.__call_api(f'pokedex?pokemon={name}')
+        if result['error']:
+            raise InputError(f'Pokémon "{name}" was not found.')
+        return Pokedex(result)
+
+    def img(self):
+        return Image()
