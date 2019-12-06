e51c4c0502fa46a2f22c7298e43abeb3b94a5513
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-06 04:54:51 +0100

    Update Main.py

--- sr_api/Main.py
+++ sr_api/Main.py
@@ -13,17 +13,17 @@ class InputError(Exception):
 
 class ApiClient:
 
-    async def __call_api(self, endpoint):
+    async def __call_api(self=None, endpoint):
         async with aiohttp.ClientSession() as c:
             async with c.get(f'https://some-random-api.ml/{endpoint}') as r:
                 return await r.json()
 
-    async def get_pokemon(self, name):
+    async def get_pokemon(self=None, name):
         try:
             return Pokedex(self.__call_api(f'pokedex?pokemon={name}')
         except KeyError:
             raise InputError(f'Pokémon "{name}" was not found.')
     
     @property
-    def img(self):
+    def img(self=None):
         return Image()
