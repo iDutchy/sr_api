dc579b45b3bc6d1c0ef79e610b2bb4b697dab2bc
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-07 01:53:23 +0100

    Update Images.py

--- sr_api/Images.py
+++ sr_api/Images.py
@@ -3,39 +3,39 @@ import json
 
 class Image:
     
-    async def __call_api(self, endpoint):
+    async def _call_api(self, endpoint):
         async with aiohttp.ClientSession() as c:
             async with c.get(f'https://some-random-api.ml/{endpoint}') as r:
                 return await r.json()
 
     async def __get_api_img(self, img):
-        base = await self.__call_api(f'img/{img}')
+        base = await self._call_api(f'img/{img}')
         return base['link']
 
     @property
     def dog(self=None):
-        return await self.__get_api_img('dog')
+        return await self._get_api_img('dog')
 
     @property
     def cat(self=None):
-        return self.__get_api_img('cat')
+        return self._get_api_img('cat')
 
     @property
     def bird(self=None):
-        return __get_api_img('bird')
+        return _get_api_img('bird')
 
     @property
     def fox(self=None):
-        return __get_api_img('fox')
+        return _get_api_img('fox')
 
     @property
     def koala(self=None):
-        return __get_api_img('koala')
+        return _get_api_img('koala')
 
     @property
     def panda(self=None):
-        return __get_api_img('panda')
+        return _get_api_img('panda')
 
     @property
     def redpanda(self=None):
-        return __get_api_img('red_panda')
+        return _get_api_img('red_panda')
