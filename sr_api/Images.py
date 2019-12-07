74ab367867e81cc39dbebc063dd955e6e7c051e9
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-07 02:39:20 +0100

    Update Images.py

--- sr_api/Images.py
+++ sr_api/Images.py
@@ -13,29 +13,29 @@ async def _get_api_img(img):
 class Image:
 
     @property
-    def dog(self=None):
+    async def dog(self=None):
         return await _get_api_img('dog')
 
     @property
-    def cat(self=None):
-        return _get_api_img('cat')
+    async def cat(self=None):
+        return await _get_api_img('cat')
 
     @property
-    def bird(self=None):
-        return _get_api_img('bird')
+    async def bird(self=None):
+        return await _get_api_img('bird')
 
     @property
-    def fox(self=None):
-        return _get_api_img('fox')
+    async def fox(self=None):
+        return await _get_api_img('fox')
 
     @property
-    def koala(self=None):
-        return _get_api_img('koala')
+    async def koala(self=None):
+        return await _get_api_img('koala')
 
     @property
-    def panda(self=None):
-        return _get_api_img('panda')
+    async def panda(self=None):
+        return await _get_api_img('panda')
 
     @property
-    def redpanda(self=None):
-        return _get_api_img('red_panda')
+    async def redpanda(self=None):
+        return await _get_api_img('red_panda')
