72171917fbb461015f6291bf7dfd7b89e1a7be02
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-08 03:44:27 +0100

    Update minecraft.py

--- sr_api/minecraft.py
+++ sr_api/minecraft.py
@@ -4,3 +4,23 @@ class Minecraft:
         self.full_uuid = data["full_uuid"]
         self.short_uuid = data["trimmed_uuid"]
         self.raw_history = data["name_history"]
+
+    async def formatted_history(self):
+        d = self.raw_history
+        
+        formatted = ""
+        for x in d:
+            for k, v in x:
+                formatted += f"{v} | {k}\n"
+                
+        return formatted
+    
+    async def reversed_formatted_history(self):
+        d = self.raw_history
+        
+        formatted = ""
+        for x in d[::-1]:
+            for k, v in x:
+                formatted += f"{v} | {k}\n"
+                
+        return formatted
