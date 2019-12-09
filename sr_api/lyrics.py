e0bbcc1431b751495a41ab5c80a29910be0ab257
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-09 01:10:09 +0100

    Update lyrics.py

--- sr_api/lyrics.py
+++ sr_api/lyrics.py
@@ -1,1 +1,12 @@
 class Lyrics:
+    def __init__(self, data):
+        self.title = data['title']
+        self.author = data['author']
+        self.lyrics = data['lyrics']
+        self.thumbnail = data['thumbnail']['genius']
+        self.link = data['links']['genius']
+        
+        
+    async def save(self):
+        with open(self.title +  ".txt", 'wb') as f:
+            return f.write(self.lyrics)
