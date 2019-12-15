9985da6739b40cd815110d2d669b45b363dcc558
iDutchy <42503862+iDutchy@users.noreply.github.com>
2019-12-15 07:28:59 +0100

    reverted changes

--- sr_api/meme.py
+++ sr_api/meme.py
@@ -1,8 +1,9 @@
-from sr_api.file import File
+import io
+
 from sr_api.http import HTTPClient
 
 
-class Meme(File):
+class Meme:
     __slots__ = ("id", "image", "caption", "category", "_http_client")
 
     def __init__(self, http_client: HTTPClient, data):
@@ -11,3 +12,19 @@ class Meme(File):
         self.caption = data['caption']
         self.category = data['category']
         self._http_client = http_client
+
+    async def read(self):
+        return await self._http_client.get(self.image)
+
+    async def save(self, fp, seek_start=True):
+        data = await self.read()
+        if isinstance(fp, io.IOBase) and fp.writable():
+            written = fp.write(data)
+
+            if seek_start:
+                fp.seek(0)
+
+            return written
+        else:
+            with open(fp, 'wb') as f:
+                return f.write(data)
