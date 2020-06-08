107efc84dab26d542720b704acf51425b573ffed
iDutchy <42503862+iDutchy@users.noreply.github.com>
2020-06-08 05:07:40 +0200

    optimizations

--- sr_api/definition.py
+++ sr_api/definition.py
@@ -1,6 +1,5 @@
 class Definition:
     __slots__ = ("word", "definition")
-
     def __init__(self, data):
-        self.word = data['word']
-        self.definition = data['definition']
+        self.word = data.get('word')
+        self.definition = data.get('definition')
