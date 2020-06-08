c3e96bcc7f99c08bdf1c697a7be5788b54df73e2
iDutchy <42503862+iDutchy@users.noreply.github.com>
2020-06-08 05:17:06 +0200

    Optimizations

--- sr_api/lyrics.py
+++ sr_api/lyrics.py
@@ -1,14 +1,13 @@
 class Lyrics:
     __slots__ = ("title", "author", "lyrics", "thumbnail", "link")
-
     def __init__(self, data):
-        self.title = data['title']
-        self.author = data['author']
-        self.lyrics = data['lyrics']
-        self.thumbnail = data['thumbnail']['genius']
-        self.link = data['links']['genius']
+        self.title = data.get('title')
+        self.author = data.get('author')
+        self.lyrics = data.get('lyrics')
+        self.thumbnail = data.get('thumbnail').get('genius')
+        self.link = data.get('links').get('genius')
         
         
-    async def save(self):
+    def save(self):
         with open(self.title +  ".txt", 'w') as f:
             return f.write(self.lyrics)
