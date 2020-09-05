54144841093f818a5238165ff028c2a135bddc8a
iDutchy <42503862+iDutchy@users.noreply.github.com>
2020-09-06 00:29:32 +0200

    oops

--- sr_api/client.py
+++ sr_api/client.py
@@ -147,7 +147,7 @@ class Client:
 
     async def filter(self, option, url):
         options = (
-            'greyscale', 'invert', 'Invertgreyscale', 'brightness', 'threshold', 'sepia', 'red', 'green', 'blue', 'blurple',
+            'greyscale', 'invert', 'invertgreyscale', 'brightness', 'threshold', 'sepia', 'red', 'green', 'blue', 'blurple',
             'pixelate', 'blur', 'gay', 'glass', 'wasted', 'triggered')
 
         if option.lower() not in options and option is not None:
