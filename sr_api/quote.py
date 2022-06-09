ca1577bad05c797a8a08b8b48ca18e62687f41f0
c0ld <83920135+c0lded@users.noreply.github.com>
2022-06-09 12:52:30 +0530

    Fix typo in sr_api/quote.py
    Fix typo in sr_api/quote.py at line 5 where character is written as characther which cause it to return None always.

--- sr_api/quote.py
+++ sr_api/quote.py
@@ -2,5 +2,5 @@ class Quote:
     __slots__ = ("quote", "character", "anime")
     def __init__(self, data):
         self.quote = data.get('sentence')
-        self.character = data.get('characther')
+        self.character = data.get('character')
         self.anime = data.get('anime')
